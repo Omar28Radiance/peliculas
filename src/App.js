ad690318ecf0739409b69640fd728bed9f123b82
Omar28Radiance <omarin0a@hotmail.com>
2022-05-11 10:25:42 -0500

    Reestructuración de código entre App, PageWrapper y Pelicula

--- src/App.js
+++ src/App.js
@@ -7,54 +7,56 @@ import peliculasJson from './peliculas.json';
 
 function App() {
 
-	const [paginaActual, setPaginaActual] = useState(1);
-	const TOTAL_POR_PAGINA = 7;
-
-	let peliculas = peliculasJson;
-
-	const buscarPeliculas = () => {
-		let url = 'https://raw.githubusercontent.com/lucasmoy-dev/Curso-de-React/main/Proyecto%202%20-%20Web%20de%20Peliculas/Peliculas/Proyecto%20Terminado/src/epliculas.js';
-
-		fetch(url, {
-			"method": 'GET', "headers": {
-				"Accept": 'application/json',
-				"Content-Type": 'application/json'
-			}
-		})
-	}
-
-	const cargarPeliculas = () => {
-		peliculas = peliculas.slice(
-			(paginaActual - 1) * TOTAL_POR_PAGINA,
-			paginaActual * TOTAL_POR_PAGINA
-		);
-	}
-
-	const getTotalPaginas = () => {
-		let cantidadTotaldePeliculas = peliculasJson.length;
-		return MATH.ceil(cantidadTotaldePeliculas / TOTAL_POR_PAGINA);
-	}
-
-	cargarPeliculas();
+	let peliculas = [
+		{
+			titulo: "oblivion (2012)",
+			calificacion: "8.1",
+			director: "Joss Whedon", 
+			actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth", 
+			fecha: "1 de Mayo 2015", 
+			duracion: "2h 21min",
+			img: "images/uploads/mv1.jpg"
+		},
+		{
+			titulo: "into the wild (2014)",
+			calificacion: "7.8",
+			director: "Anthony Russo, Joe Russo", 
+			actores: "Chris Evans, Samuel L. Jackson, Scarlett Johansson", 
+			fecha: "1 de Mayo 2015", 
+			duracion: "2h 21min",
+			img: "images/uploads/mv2.jpg"
+		}
+	];
+//Lo que hace la funcion es recorrer el array de peliculas
+	// for (var i = 0; i < peliculas.length; i++)
+	// peliculas.forEach(pelicula => {
+	// 	alert(pelicula.titulo)
+	// })
+//En lugar de forEach, se puede usar el map para recorrer el array. El forEach recorre cada una de las peliculas y ejecuta una acción. En cambio el map recorre cada una de las peliculas y a la vez va a devolver alguna cosa. En este caso devualve código html para renderizar. Por ejemplo cuando se active la arrow function y recorra cada una de las peliculas, regresará el string de "asdas",
+	var arreglo = peliculas.map(pelicula => {
+		alert(pelicula.titulo)
+		//return "asdas";
+	})
 
 	return (
 		<PageWrapper>
 
-			{peliculas.map(pelicula =>
-				<Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion}
-					director={pelicula.director} actores={pelicula.actores} fecha={pelicula.fecha}
-					duracion={pelicula.duracion} img={pelicula.img}>
-					{pelicula.descripcion}
+			{peliculas.map(pelicula => 
+				<Pelicula titulo="oblivion (2012)" calificacion="8.1"
+					director="Joss Whedon" actores="Robert Downey Jr., Chris Evans, 
+					Chris Hemsworth" fecha="1 de Mayo 2015" duracion="2h 21min"
+					img="images/uploads/mv1.jpg">
+					Earth's mightiest heroes must come together and learn to fight as a team
+					if they are to stop the mischievous Loki and his alien army from
+					enslaving humanity...
 				</Pelicula>
+
 			)}
 
-			<Paginacion pagina={paginaActual} total={getTotalPaginas} 
-			onChange={(pagina) => {
-				setPaginaActual(pagina)
-			}} />
 
 		</PageWrapper>
 	);
 }
 
 export default App;
+//Continuar video desde minuto 01:41:05
--- src/Pelicula.js
+++ src/Pelicula.js
@@ -1,4 +1,5 @@
-function Pelicula(props) {
+export default function Pelicula(props) {
+    
     return (
         <div className="movie-item-style-2">
             <img src={props.img} alt="" />
@@ -10,10 +11,11 @@ function Pelicula(props) {
                 </h6>
                 <p className="rate">
                     <i className="ion-android-star"></i>
-                <span>{props.calificacion}</span> 
+                    <span>{props.calificacion}</span> 
                 </p>
                 <p className="describe">{props.children}</p>
-                <p className="run-time"> Duración: {props.duracion}    .     <span>MMPA: PG-13 </span>    .     
+                <p className="run-time"> Duración: {props.duracion}    .     <span>MMPA: PG-13 </
+                span>    .     
                 <span>Fecha: {props.fecha}</span></p>
                 <p>Director: 
                     <a href="#">{props.director}</a>
@@ -26,4 +28,3 @@ function Pelicula(props) {
     )
 }
 
-export default Pelicula;
