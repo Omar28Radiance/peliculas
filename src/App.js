d6afef7e1a8049e8490a164dc8382f5e669cb7ca
Omar28Radiance <omarin0a@hotmail.com>
2022-05-11 13:05:11 -0500

    Integración de peliculas al array de "peliculas"

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
 			actores: "Robert Downey Jr., Chris Evans, Chris Hemsworth", 
 			fecha: "1 de Mayo 2015", 
 			duracion: "2h 21min",
-			img: "images/uploads/mv1.jpg"
+			img: "images/uploads/mv1.jpg",
+			descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
 		},
 		{
 			titulo: "into the wild (2014)",
@@ -24,7 +25,38 @@ function App() {
 			actores: "Chris Evans, Samuel L. Jackson, Scarlett Johansson", 
 			fecha: "1 de Mayo 2015", 
 			duracion: "2h 21min",
-			img: "images/uploads/mv2.jpg"
+			img: "images/uploads/mv2.jpg",
+			descripcion: "Earth's mightiest heroes must come together and learn to fight as a team if they are to stop the mischievous Loki and his alien army from enslaving humanity..."
+		},
+		{
+			titulo: "BLADE RUNNER (2015)",
+			calificacion: "7.3",
+			director: "Peyton Reed", 
+			actores: "Paul Rudd, Michael Douglas", 
+			fecha: "1 de Mayo 2015", 
+			duracion: "2h 21min",
+			img: "images/uploads/mv3.jpg",
+			descripcion: "Armed with a super-suit with the astonishing ability to shrink in scale but increase in strength, cat burglar Scott Lang must embrace his inner hero and help..."
+		},
+		{
+			titulo: "MULHOLLAND PRIDE (2013)",
+			calificacion: "7.2",
+			director: "Shane Black", 
+			actores: "Robert Downey Jr., Guy Pearce, Don Cheadle", 
+			fecha: "1 de Mayo 2015", 
+			duracion: "2h 21min",
+			img: "images/uploads/mv4.jpg",
+			descripcion: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
+		},
+		{
+			titulo: "SKYFALL: EVIL OF BOSS (2013)",
+			calificacion: "7.0",
+			director: "Alan Taylor", 
+			actores: "Chris Hemsworth, Natalie Portman, Tom Hiddleston", 
+			fecha: "1 de Mayo 2015", 
+			duracion: "2h 21min",
+			img: "images/uploads/mv5.jpg",
+			descripcion: "When Tony Stark's world is torn apart by a formidable terrorist called the Mandarin, he starts an odyssey of rebuilding and retribution."
 		}
 	];
 //Lo que hace la funcion es recorrer el array de peliculas
@@ -45,9 +77,7 @@ function App() {
 				<Pelicula titulo={pelicula.titulo} calificacion={pelicula.calificacion}
 					director={pelicula.director} actores={pelicula.actores} fecha={pelicula.fecha} duracion={pelicula.duracion}
 					img={pelicula.img}>
-					Earth's mightiest heroes must come together and learn to fight as a team
-					if they are to stop the mischievous Loki and his alien army from
-					enslaving humanity...
+					{pelicula.descripcion}
 				</Pelicula>
 
 			)}
@@ -58,4 +88,4 @@ function App() {
 }
 
 export default App;
-//Continuar video desde minuto 01:41:05
+//Continuar video desde minuto 01:45:41
