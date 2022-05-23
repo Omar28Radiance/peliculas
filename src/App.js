e41257fe961774fb21a992f8cd1083eda7d7fc74
Omar28Radiance <omarin0a@hotmail.com>
2022-05-23 11:02:38 -0500

    Implementación de switch y route con ListadoPeliculas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import {  useEffect, useState } from 'react';
-// import { useEffect } from 'react/cjs/react.production.min';
 import './App.css';
 import ListadoPeliculas from './ListadoPeliculas';
-import PageWrapper from './PageWrapper';
-import Paginacion from './Paginacion';
-import Pelicula from './Pelicula';
 
 function App() {
 
 	return (
-		<ListadoPeliculas />
+		<switch>
+			<Route>
+				<ListadoPeliculas />
+			</Route>
+		</switch>
 	);
-
-	
 }
 
 export default App;
